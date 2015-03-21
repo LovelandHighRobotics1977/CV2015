4352d6f8aad077d7023ee09305163de10df8ffba
Curtis Nottberg <curtisn@comcast.net>
2015-03-21 16:06:18 -0600

    Add true daemon code to allow start/stop at boot time.  Change the name of the executable to cvs1977.

--- exampleCode/WebTest/Deamon.cpp
+++ exampleCode/WebTest/Deamon.cpp
@@ -5,6 +5,12 @@
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+
 #include <fcntl.h>
 #include <microhttpd.h>
 
@@ -18,6 +24,12 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+
+#include <libdaemon/dfork.h>
+#include <libdaemon/dsignal.h>
+#include <libdaemon/dlog.h>
+#include <libdaemon/dpid.h>
+#include <libdaemon/dexec.h>
 
 using namespace std;
 
@@ -44,13 +56,13 @@ bool startCamera()
     //Camera.set ( cv::CAP_PROP_FORMAT, CV_8UC1 ); // Do Gray Scale
     //Camera.set ( cv::CAP_PROP_EXPOSURE, ?  );
 
-    cout<<"Connecting to camera"<<endl;
+    //cout<<"Connecting to camera"<<endl;
     if ( !Camera.open() ) {
         cerr<<"Error opening camera"<<endl;
         return true;
     }
 
-    cout<<"Connected to camera ="<<Camera.getId() <<endl;
+    //cout<<"Connected to camera ="<<Camera.getId() <<endl;
 
     return false;
 }
@@ -64,6 +76,7 @@ class TemplateObj
 {
     private:
         vector< cv::Point > idealContour;
+	double aspect;
         
     public:
 
@@ -74,6 +87,9 @@ class TemplateObj
         void addPoint( unsigned int x, unsigned int y );
 
         vector< cv::Point > getIdealContour();
+
+	void setAspect( double asp );
+	double getAspect();
 };
 
 TemplateObj::TemplateObj()
@@ -103,8 +119,20 @@ TemplateObj::addPoint( unsigned int x, unsigned int y )
 vector< cv::Point > 
 TemplateObj::getIdealContour()
 {
-    std::cout << "Contourpoints: " << idealContour.size() << std::endl;
+    //std::cout << "Contourpoints: " << idealContour.size() << std::endl;
     return idealContour;
+}
+
+void
+TemplateObj::setAspect( double asp) 
+{
+    aspect = asp;
+}
+
+double
+TemplateObj::getAspect()
+{
+    return aspect;
 }
 
 TemplateObj gToteSide;
@@ -151,6 +179,8 @@ class SceneObj
         void setType( SCNOBJ_TYPE_T newType );
         SCNOBJ_TYPE_T getType();
         std::string getTypeAsStr();
+
+        double getNumStacked();
 };
 
 SceneObj::SceneObj()
@@ -250,6 +280,25 @@ SceneObj::getMatchConfidence()
     return matchConfidence;
 }
 
+double
+SceneObj::getNumStacked()
+{
+    switch( type )
+    {
+        case SCNOBJ_TYPE_UNKNOWN:
+	    return 0;
+
+	case SCNOBJ_TYPE_Y_BLOB:
+	    return 0;
+	
+	case SCNOBJ_TYPE_Y_TOTE_LONGSIDE:
+	    return (minRect.size.height / minRect.size.width) / ( 26.9 / 12.1 );
+
+	case SCNOBJ_TYPE_Y_TOTE_END:
+	    return (minRect.size.height / minRect.size.width) / ( 16.9 / 12.1 );
+     }
+}
+
 class Scene
 {
     private:
@@ -282,7 +331,7 @@ class Scene
 
 Scene::Scene()
 {
-    minObjectArea = 10000;
+    minObjectArea = 10000; //10000
 }
 
 Scene::~Scene()
@@ -293,7 +342,7 @@ Scene::~Scene()
 bool 
 Scene::retrieveImage()
 {
-    cout << "capturing" << endl;
+    //cout << "capturing" << endl;
 
     if( !Camera.grab() )
     {
@@ -314,7 +363,7 @@ Scene::thresholdImage()
     cv::blur( image, image, cv::Size(3,3) );
 
     //cv::inRange( src_gray, cv::Scalar(20, 100, 100), cv::Scalar(30, 255, 255), src_gray);
-    // Yellow bin
+    // Yellow bin 23, 100, 100 : 28, 255, 255
     cv::inRange( image, cv::Scalar(23, 100, 100), cv::Scalar(28, 255, 255), image);
     
     //cv::inRange( src_gray, cv::Scalar(20, 100, 100), cv::Scalar(200, 255, 255), src_gray);
@@ -347,7 +396,7 @@ Scene::findObjects()
     for( int i = 0; i< contours.size(); i++ )
     {
         double CA = contourArea( contours[i], false );
-        printf( "Countour( %d ) - Area: %g \n", i, CA );
+        //printf( "Countour( %d ) - Area: %g \n", i, CA );
         if( CA >= minObjectArea )
         {
             SceneObj obj; 
@@ -358,11 +407,11 @@ Scene::findObjects()
 
             for( int j = 0; j < contours[i].size(); j++ )
             {
-                std::cout << "Contour Point " << j << ":" << contours[i][j].x << ", " << contours[i][j].y << std::endl;
+                //std::cout << "Contour Point " << j << ":" << contours[i][j].x << ", " << contours[i][j].y << std::endl;
             }
 
             double matchVal = matchShapes( contours[ i ], gToteSide.getIdealContour(), 1, 0 );
-            std::cout << "Tote Side matchVal: " << matchVal << std::endl;
+            //std::cout << "Tote Side matchVal: " << matchVal << std::endl;
 
             if( matchVal < 0.2 )
             {
@@ -371,7 +420,7 @@ Scene::findObjects()
             }
 
             matchVal = matchShapes( contours[ i ], gToteEnd.getIdealContour(), 1, 0 );
-            std::cout << "Tote End matchVal: " << matchVal << std::endl;
+            //std::cout << "Tote End matchVal: " << matchVal << std::endl;
 
             if( matchVal < 0.2 )
             {
@@ -380,10 +429,10 @@ Scene::findObjects()
             }
 
             matchVal = matchShapes( contours[ i ], gToteAngleRight.getIdealContour(), 1, 0 );
-            std::cout << "Tote AngleRight matchVal: " << matchVal << std::endl;
+            //std::cout << "Tote AngleRight matchVal: " << matchVal << std::endl;
 
             matchVal = matchShapes( contours[ i ], gToteAngleLeft.getIdealContour(), 1, 0 );
-            std::cout << "Tote AngleLeft matchVal: " << matchVal << std::endl;
+            //std::cout << "Tote AngleLeft matchVal: " << matchVal << std::endl;
 
             objList.push_back( obj );
         }
@@ -447,10 +496,13 @@ Scene::getObjectListAsContent()
 
         rspData << "<rbox-cx>" << it->getMinRect().center.x << "</rbox-cx>";
         rspData << "<rbox-cy>" << it->getMinRect().center.y << "</rbox-cy>";
-        rspData << "<rbox-w>" << it->getMinRect().size.width << "</rbox-w>";
-        rspData << "<rbox-h>" << it->getMinRect().size.height << "</rbox-h>";
+        //Width and height appear to be consistently reversed, perhaps on account of the, being rotated rects.
+	//Do more testing on this later.  For now, manually reverse the two axes.
+        rspData << "<rbox-w>" << it->getMinRect().size.height << "</rbox-w>";
+        rspData << "<rbox-h>" << it->getMinRect().size.width << "</rbox-h>";
         rspData << "<rbox-angle>" << it->getMinRect().angle << "</rbox-angle>";
         rspData << "<rbox-aspect>" << (it->getMinRect().size.width/it->getMinRect().size.height) << "</rbox-aspect>";
+	rspData << "<est-stacked>" << (it->getNumStacked()) << "</est-stacked>";
 
         cv::Point2f rectPts[4];
         it->getMinRect().points( rectPts );
@@ -535,7 +587,7 @@ answer_to_connection( void *cls, struct MHD_Connection *connection, const char *
     struct MHD_Response *response;
     int ret;
 
-    printf( "Request URL: %s\n", url );
+    //printf( "Request URL: %s\n", url );
 
 #if 0
     const char *page = "<html><body>Hello, browser!</body></html>";
@@ -557,7 +609,7 @@ answer_to_connection( void *cls, struct MHD_Connection *connection, const char *
 
         bufLength = getImage();
 
-        printf( "Sending image -- size: %d\n", bufLength );
+        //printf( "Sending image -- size: %d\n", bufLength );
 
         if( bufLength == 0 )
             return MHD_NO;
@@ -579,7 +631,7 @@ answer_to_connection( void *cls, struct MHD_Connection *connection, const char *
 
         bufLength = getToteImage();
 
-        printf( "Sending image -- size: %d\n", bufLength );
+        //printf( "Sending image -- size: %d\n", bufLength );
 
         if( bufLength == 0 )
             return MHD_NO;
@@ -601,7 +653,7 @@ answer_to_connection( void *cls, struct MHD_Connection *connection, const char *
 
         bufLength = getContourImage();
 
-        printf( "Sending image -- size: %d\n", bufLength );
+        //printf( "Sending image -- size: %d\n", bufLength );
 
         if( bufLength == 0 )
             return MHD_NO;
@@ -661,7 +713,7 @@ answer_to_connection( void *cls, struct MHD_Connection *connection, const char *
             return MHD_NO;
     }
 
-    printf( "Sending image -- size: %d\n", sbuf.st_size );
+    //printf( "Sending image -- size: %d\n", sbuf.st_size );
  
     response = MHD_create_response_from_fd( sbuf.st_size, fd );
     MHD_add_response_header( response, "Content-Type", MIMETYPE );
@@ -687,6 +739,7 @@ answer_to_connection( void *cls, struct MHD_Connection *connection, const char *
 
 }
 
+#if 0
 int
 main ()
 {
@@ -704,6 +757,7 @@ main ()
     gToteSide.addPoint( 35, 239 );
     gToteSide.addPoint( 20, 30 );
     gToteSide.addPoint( 0, 30 );
+    gToteSide.setAspect( 26.9 / 12.1 );
 
     gToteEnd.addPoint( 0, 0 );
     gToteEnd.addPoint( 336, 0 );
@@ -713,6 +767,7 @@ main ()
     gToteEnd.addPoint( 30, 243 );
     gToteEnd.addPoint( 15, 30 );
     gToteEnd.addPoint( 0, 30 );
+    gToteEnd.setAspect( 16.9 / 12.1 );
 
     gToteAngleRight.addPoint( 0, 66 );
     gToteAngleRight.addPoint( 380, 0 );
@@ -743,5 +798,219 @@ main ()
     stopCamera();
 
     return 0;
+}
+#endif
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+
+    /* Reset signal handlers */
+    if (daemon_reset_sigs(-1) < 0) {
+        daemon_log(LOG_ERR, "Failed to reset all signal handlers: %s", strerror(errno));
+        return 1;
+    }
+
+    /* Unblock signals */
+    if (daemon_unblock_sigs(-1) < 0) {
+        daemon_log(LOG_ERR, "Failed to unblock all signals: %s", strerror(errno));
+        return 1;
+    }
+
+    /* Set indetification string for the daemon for both syslog and PID file */
+    daemon_pid_file_ident = daemon_log_ident = daemon_ident_from_argv0(argv[0]);
+
+    /* Check if we are called with -k parameter */
+    if (argc >= 2 && !strcmp(argv[1], "-k")) {
+        int ret;
+
+        /* Kill daemon with SIGTERM */
+
+        /* Check if the new function daemon_pid_file_kill_wait() is available, if it is, use it. */
+        if ((ret = daemon_pid_file_kill_wait(SIGTERM, 5)) < 0)
+            daemon_log(LOG_WARNING, "Failed to kill daemon: %s", strerror(errno));
+
+        return ret < 0 ? 1 : 0;
+    }
+
+    /* Check that the daemon is not rung twice a the same time */
+    if ((pid = daemon_pid_file_is_running()) >= 0) {
+        daemon_log(LOG_ERR, "Daemon already running on PID file %u", pid);
+        return 1;
+    }
+
+    /* Prepare for return value passing from the initialization procedure of the daemon process */
+    if (daemon_retval_init() < 0) {
+        daemon_log(LOG_ERR, "Failed to create pipe.");
+        return 1;
+    }
+
+    /* Do the fork */
+    if ((pid = daemon_fork()) < 0) {
+
+        /* Exit on error */
+        daemon_retval_done();
+        return 1;
+
+    } else if (pid) { /* The parent */
+        int ret;
+
+        /* Wait for 20 seconds for the return value passed from the daemon process */
+        if ((ret = daemon_retval_wait(20)) < 0) {
+            daemon_log(LOG_ERR, "Could not recieve return value from daemon process: %s", strerror(errno));
+            return 255;
+        }
+
+        daemon_log(ret != 0 ? LOG_ERR : LOG_INFO, "Daemon returned %i as return value.", ret);
+        return ret;
+
+    } else { /* The daemon */
+        int fd, quit = 0;
+        fd_set fds;
+        struct MHD_Daemon *daemon;
+
+        /* Close FDs */
+        if (daemon_close_all(-1) < 0) {
+            daemon_log(LOG_ERR, "Failed to close all file descriptors: %s", strerror(errno));
+
+            /* Send the error condition to the parent process */
+            daemon_retval_send(1);
+            goto finish;
+        }
+
+        /* Create the PID file */
+        if (daemon_pid_file_create() < 0) {
+            daemon_log(LOG_ERR, "Could not create PID file (%s).", strerror(errno));
+            daemon_retval_send(2);
+            goto finish;
+        }
+
+        /* Initialize signal handling */
+        if (daemon_signal_init(SIGINT, SIGTERM, SIGQUIT, SIGHUP, 0) < 0) {
+            daemon_log(LOG_ERR, "Could not register signal handlers (%s).", strerror(errno));
+            daemon_retval_send(3);
+            goto finish;
+        }
+
+        /*... do some further init work here */
+        // Try to open the camera
+        if( startCamera() )
+        {
+            daemon_log(LOG_ERR, "Could not open camera device.");
+            daemon_retval_send(40);
+            goto finish;
+        }
+
+        gToteSide.addPoint( 0, 0 );
+        gToteSide.addPoint( 533, 0 );
+        gToteSide.addPoint( 533, 30 );
+        gToteSide.addPoint( 515, 30 );
+        gToteSide.addPoint( 490, 239 );
+        gToteSide.addPoint( 35, 239 );
+        gToteSide.addPoint( 20, 30 );
+        gToteSide.addPoint( 0, 30 );
+        gToteSide.setAspect( 26.9 / 12.1 );
+
+        gToteEnd.addPoint( 0, 0 );
+        gToteEnd.addPoint( 336, 0 );
+        gToteEnd.addPoint( 336, 30 );
+        gToteEnd.addPoint( 316, 30 );
+        gToteEnd.addPoint( 306, 243 );
+        gToteEnd.addPoint( 30, 243 );
+        gToteEnd.addPoint( 15, 30 );
+        gToteEnd.addPoint( 0, 30 );
+        gToteEnd.setAspect( 16.9 / 12.1 );
+
+        gToteAngleRight.addPoint( 0, 66 );
+        gToteAngleRight.addPoint( 380, 0 );
+        gToteAngleRight.addPoint( 538, 47 );
+        gToteAngleRight.addPoint( 516, 52 );
+        gToteAngleRight.addPoint( 500, 262 );
+        gToteAngleRight.addPoint( 60, 262 );
+        gToteAngleRight.addPoint( 15, 70 );
+
+        gToteAngleLeft.addPoint( 538, 66 );    
+        gToteAngleLeft.addPoint( 158, 0 );     
+        gToteAngleLeft.addPoint( 0, 47 ); 
+        gToteAngleLeft.addPoint( 22, 52 );
+        gToteAngleLeft.addPoint( 30, 262 );
+        gToteAngleLeft.addPoint( 488, 262 );
+        gToteAngleLeft.addPoint( 508, 70 ); 
+
+        daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, &answer_to_connection, NULL, MHD_OPTION_END );
+
+        if( NULL == daemon )
+        {
+            daemon_log(LOG_ERR, "Could not start the microhttpd webserver.");
+            daemon_retval_send(41);
+            goto finish;
+        }
+
+        /* Send OK to parent process */
+        daemon_retval_send(0);
+
+        daemon_log(LOG_INFO, "Sucessfully started");
+
+        /* Prepare for select() on the signal fd */
+        FD_ZERO(&fds);
+        fd = daemon_signal_fd();
+        FD_SET(fd, &fds);
+
+        while (!quit) {
+            fd_set fds2 = fds;
+
+            /* Wait for an incoming signal */
+            if (select(FD_SETSIZE, &fds2, 0, 0, 0) < 0) {
+
+                /* If we've been interrupted by an incoming signal, continue */
+                if (errno == EINTR)
+                    continue;
+
+                daemon_log(LOG_ERR, "select(): %s", strerror(errno));
+                break;
+            }
+
+            /* Check if a signal has been recieved */
+            if (FD_ISSET(fd, &fds2)) {
+                int sig;
+
+                /* Get signal */
+                if ((sig = daemon_signal_next()) <= 0) {
+                    daemon_log(LOG_ERR, "daemon_signal_next() failed: %s", strerror(errno));
+                    break;
+                }
+
+                /* Dispatch signal */
+                switch (sig) {
+
+                    case SIGINT:
+                    case SIGQUIT:
+                    case SIGTERM:
+                        daemon_log(LOG_WARNING, "Got SIGINT, SIGQUIT or SIGTERM.");
+                        quit = 1;
+                        break;
+
+                    case SIGHUP:
+                        daemon_log(LOG_INFO, "Got a HUP");
+                        daemon_exec("/", NULL, "/bin/ls", "ls", (char*) NULL);
+                        break;
+
+                }
+            }
+        }
+
+        /* Do a cleanup */
+finish:
+        daemon_log(LOG_INFO, "Exiting...");
+        daemon_retval_send(255);
+        daemon_signal_done();
+        daemon_pid_file_remove();
+
+        MHD_stop_daemon (daemon);
+        
+        // Close the camera
+        stopCamera();
+
+        return 0;
+    }
 }
 
